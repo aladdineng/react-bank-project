ab015fdd030c38a3a23eae043965763a10e66036
soud <soud9949ale@gmail.com>
2024-05-29 19:47:02 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+
+
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
-      <Navbar /> 
-      <Home  />
+     
+ 
+      <Register  />
     </div>
   );
 }
